7638eb67b9d67ea05deb9e272255f1d984784059
("Mridul") <93548699+Zledme@users.noreply.github.com>
2022-12-15 21:03:58 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pip install pyserini faiss-cpu transformers
-pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cpu
+pip install torch --extra-index-url https://download.pytorch.org/whl/cpu
 wget https://rgw.cs.uwaterloo.ca/JIMMYLIN-bucket0/pyserini-indexes/lucene-index.beir-v1.0.0-dbpedia-entity-flat.20220501.1842ee.tar.gz
 tar zxvf lucene-index.beir-v1.0.0-dbpedia-entity-flat.20220501.1842ee.tar.gz
 git lfs install
