541185dcc8b2a4a4c6a0c30039ab65e112cf9fd8
("Mridul") <93548699+Zledme@users.noreply.github.com>
2022-12-15 20:38:04 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pip install pyserini faiss-cpu transformers torch
+pip install pyserini faiss-cpu transformers torch --force-reinstall --no-cache-dir
 wget https://rgw.cs.uwaterloo.ca/JIMMYLIN-bucket0/pyserini-indexes/lucene-index.beir-v1.0.0-dbpedia-entity-flat.20220501.1842ee.tar.gz
 tar zxvf lucene-index.beir-v1.0.0-dbpedia-entity-flat.20220501.1842ee.tar.gz
 git lfs install
